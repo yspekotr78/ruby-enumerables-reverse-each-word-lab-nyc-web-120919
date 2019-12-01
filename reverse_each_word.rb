747decccfa4314af96494112cd8422776d316bba
Yury Spektor <yspektor78@gmail.com>
2019-12-01 02:05:48 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,21 @@
+def reverse_each_word(string)
+  new_string = string.split(" ")
+  reversed_string = new_string.each {|x| x.reverse!}
+  return reversed_string.join(" ")
+  
+end
+def reverse_each_word(string)
+  newer_string = string.split(" ")
+  newer_string.collect{|x| x.reverse!}
+  return newer_string.join(" ")
+end
+
+#def reverse_each_word(array)
+ # array.split(" ").collect {|x| x.reverse!}.join(" ")
+#end
+
+#def reverse_each_word(array)
+#  new_array = array.split(" ")
+#  new_array.collect {|x| x.reverse!}
+#  new_array.join(" ")
+#end
